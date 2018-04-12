2e8af45178aab58886a1c2e527f17db7948c2371
Carlo Schneider <uab.carlo@gmail.com>
2018-04-12 15:02:53 -0300

    Init :rocket:

---
+++ README.md
@@ -0,0 +1,23 @@
+# eslint-config-xo-avanti
+
+## Install
+
+```
+$ npm install --save-dev xo eslint-config-xo-avanti
+```
+
+## Usage
+
+Add some configs to your `package.json`:
+
+```json
+{
+  "name": "my-awesome-project",
+  "scripts": {
+    "lint": "xo"
+  },
+  "xo": {
+    "extends": "xo-avanti"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+  'extends': [
+    'xo',
+    'xo-space/esnext'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 2017
+  },
+  'globals': {
+    '$': true,
+    'describe': true,
+    'test': true,
+    'expect': true,
+    'window': true
+  },
+  'rules': {
+    'semi': [2, 'never'],
+    'capitalized-comments': 0,
+    'object-curly-spacing': [2, 'always'],
+    'array-bracket-spacing': [0, 'always'],
+    'space-before-function-paren': ['error', 'always']
+  }
+}
